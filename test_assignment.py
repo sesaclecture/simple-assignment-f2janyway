dae4df49d1480e24ca901dc26c4aef4a8b27e342
yegyu <f2janyway@gmail.com>
2025-08-27 10:41:23 +0900

    merge from upstream

--- test_assignment.py
+++ test_assignment.py
@@ -6,9 +6,8 @@ def test_name_type():
 def test_age_type():
     assert isinstance(assignment.age, int)
 
-def test_numbers_content():
-    assert 2 in assignment.numbers
+def test_numbers_type():
     assert len(assignment.numbers) == 3
 
-def test_is_student():
-    assert assignment.is_student is True
+def test_is_students():
+    assert assignment.is_student is False 
